9354f0ff0acbe863d723c8931eda2855371de650
rkolx <olx.rko.o@gmail.com>
2021-11-09 09:55:10 +0900

    Create oddGame.v1

---
+++ week1/src/IndianName.java
@@ -0,0 +1,26 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Scanner;
+
+
+public class IndianName {
+    public static void main(String[] args) {
+
+        String[] year = {"시끄러운", "푸른", "적색", "조용한", "웅크린", "백색", "지혜로운", "용감한", "날카로운", "욕심많은"};
+
+        String[] month = {"늑대", "태양", "양", "매", "황소", "불꽃", "나무", "달빛", "말", "돼지", "하늘", "바람"};
+
+        String[] day = {"와(과) 함께 춤을", "의 기상", "은(는) 그림자속에", "", "", "", "의 환생", "의 죽음", "아래에서", "을(를) 보라",
+                "이(가) 노래하다", "그림자", "의 일격", "에게 쫓기는 남자", "의 행진", "의 왕", "의 유령", "을(를) 죽인자", "는(은) 맨날 잠잔다",
+                "처럼", "의 고향", "의 천사", "은(는) 나의 친구", "노래", "정령", "의 파수꾼", "의 악마", "와(과)같은 사나이", "을(를) 쓰러트린자", "의 혼", "은(는) 말이 없다"};
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("생년월일이 어떻게 되니 ex.19950329");
+        int birth = sc.nextInt();
+
+
+
+
+
+    }
+}
---
+++ week2/src/OddOrEven.java
@@ -0,0 +1,100 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class OddOrEven {
+
+    public static class Player { //static 다시 보자
+        public String playName;
+        public int score;
+
+        public Player(String name, int score) {
+            this.playName = name;
+            this.score = score;
+        }
+
+        public void addScore(int socre) {
+            this.score += socre;
+        }
+
+        public void delScore(int score) {
+            this.score -= score;
+        }
+
+        @Override
+        public String toString() {
+            return "player : " + playName  +
+                    ", score : " + score;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+
+        int round = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("이름을 정해줘 : ");
+        String inputPlayer = sc.next();
+
+        Player player = new Player(inputPlayer, 100);
+        Player player1 = new Player("player"+round, 120); //플레이어는 객체 생성할 수 있도록 만들자
+
+
+        do {
+            if (player1.score == 0) {
+                System.out.println("nextplayer");
+                round = round + 1;
+                player1 = new Player("player"+round, player.score+2);
+            }
+
+
+            System.out.println(player+"   vs   "+player1);
+
+            Random random= new Random();
+            int randomNumber = random.nextInt(10)+1;
+            System.out.println(randomNumber);//임시
+
+            int resultNum = 0;
+            if (randomNumber % 2 == 0) {
+                resultNum = 0;
+                System.out.println(resultNum);//삭제
+            } else {
+                resultNum = 1;
+                System.out.println(resultNum);//삭제
+            }
+
+
+            System.out.println("1(홀수), 0(짝수) 골라");
+            int choiceNum = sc.nextInt();
+            System.out.println("얼마 걸래");
+            int bet = sc.nextInt();
+
+
+            if (choiceNum == resultNum) {
+                player.addScore(bet);
+                player1.delScore(bet);
+                System.out.println("이겼다. 너의 점수는 : "+ player.score);
+                System.out.println("이겼다. 너의 점수는 : "+ player1.score);
+
+            }else {
+                player.delScore(bet);
+                player1.addScore(bet);
+                System.out.println("졌다. 너의 점수는 : "+ player.score);
+                System.out.println("이겼다. 너의 점수는 : "+ player1.score);
+            }
+
+
+
+        } while (player.score != 0 && round < 8);
+
+
+    }
+
+
+
+
+
+
+}
