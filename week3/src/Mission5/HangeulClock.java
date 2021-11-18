e2e7117dcd9c37c8ac01add2397387b7fbc1adae
rkolx <olx.rko.o@gmail.com>
2021-11-18 10:44:15 +0900

    시간 맵핑 및 임시 기능 구현

--- week3/src/Mission5/HangeulClock.java
+++ week3/src/Mission5/HangeulClock.java
@@ -11,6 +11,7 @@ public class HangeulClock {
 
         Clock cl = new Clock();
         cl.setHour();
+        System.out.println();//임시
 
     }
 }
@@ -19,7 +20,7 @@ class Clock {
         LocalTime time = LocalTime.now();
         int hour = time.getHour();
         int minute = time.getMinute();
-        String[][] hangule =
+        String[][] hangeul =
                 {{"한", "두", "세", "네", "다", "섯",},
                         {"여", "섯", "일", "곱", "여", "덟",},
                         {"아", "홉", "열", "한", "두", "시",},
@@ -38,32 +39,28 @@ class Clock {
 
             HashMap<Integer, Integer[]> setDate = new HashMap<>();
 
-            setDate.put(1, new Integer[]{0, 0});
-            setDate.put(2, new Integer[]{0, 1});
-            setDate.put(3, new Integer[]{0, 2});
-            setDate.put(4, new Integer[]{0, 3});
-            setDate.put(5, new Integer[]{0, 4, 0, 5});
-            setDate.put(6, new Integer[]{1, 0, 1, 1});
-            setDate.put(7, new Integer[]{1, 2, 1, 3});
-            setDate.put(8, new Integer[]{1, 4, 1, 5});
-            setDate.put(9, new Integer[]{2, 0, 2, 1});
-            setDate.put(10, new Integer[]{2, 2});
-            setDate.put(11, new Integer[]{2, 2, 2, 3});
-            setDate.put(12, new Integer[]{2, 2, 2, 4});
+            setDate.put(1, new Integer[]{0,0, 2,5});
+            setDate.put(2, new Integer[]{0,1, 2,5});
+            setDate.put(3, new Integer[]{0,2, 2,5});
+            setDate.put(4, new Integer[]{0,3, 2,5});
+            setDate.put(5, new Integer[]{0,4, 0,5, 2,5});
+            setDate.put(6, new Integer[]{1,0, 1,1, 2,5});
+            setDate.put(7, new Integer[]{1,2, 1,3, 2,5});
+            setDate.put(8, new Integer[]{1,4, 1,5, 2,5});
+            setDate.put(9, new Integer[]{2,0, 2,1, 2,5});
+            setDate.put(10, new Integer[]{2,2, 2,5});
+            setDate.put(11, new Integer[]{2,2, 2,3, 2,5});
+            setDate.put(12, new Integer[]{2,2, 2,4, 2,5});
 
             Integer[] temp = setDate.get(hour);
             System.out.println("----change time num to hangeul-----");//임시
             System.out.println("currnet time: " + hour + "시");//임시
 
             for (int i = 0; i < temp.length; i = i + 2) {
-                System.out.println(hangule[temp[i]][temp[i + 1]] + "시");
+                System.out.print(hangeul[temp[i]][temp[i + 1]]);
 
             }
         }
 
-    //분 파싱
-        public void setMinute() {
-            String tens = String.valueOf(minute).substring(0,1); //분 십의 자리
-            String units = String.valueOf(minute).substring(1); //분 일의 자리
-        }
+
 }
