613443f34a07a7b7bd4e7076e7b615349c064e43
rkolx <olx.rko.o@gmail.com>
2021-11-22 22:55:14 +0900

    feat: 한글시계 hour 기능

--- week3/src/Mission5/HangeulClock.java
+++ week3/src/Mission5/HangeulClock.java
@@ -1,11 +1,18 @@
 package Mission5;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
 public class HangeulClock {
 
 
 }
 
 class Clock {
+
+    Integer[] temp1;
+    Integer[] temp2;
 
     final String RESET = "\033[0m";  // Text Reset    public static final String BLACK_BOLD_BRIGHT = "\033[1;90m"; // BLACK
     final String RED_BOLD_BRIGHT = "\033[1;91m";   // RED
@@ -24,11 +31,37 @@ class Clock {
                     {"오", "육", "칠", "팔", "구", "분",}};
 
 
+    public void setHour() {
+        String pattern = "h";
+        SimpleDateFormat simple = new SimpleDateFormat(pattern);
+        Integer hour = Integer.parseInt(simple.format(new Date()));
+
+        HashMap<Integer, Integer[]> setDate = new HashMap<>();
+        setDate.put(1, new Integer[]{0, 0, 2, 5});
+        setDate.put(2, new Integer[]{0, 1, 2, 5});
+        setDate.put(3, new Integer[]{0, 2, 2, 5});
+        setDate.put(4, new Integer[]{0, 3, 2, 5});
+        setDate.put(5, new Integer[]{0, 4, 0, 5, 2, 5});
+        setDate.put(6, new Integer[]{1, 0, 1, 1, 2, 5});
+        setDate.put(7, new Integer[]{1, 2, 1, 3, 2, 5});
+        setDate.put(8, new Integer[]{1, 4, 1, 5, 2, 5});
+        setDate.put(9, new Integer[]{2, 0, 2, 1, 2, 5});
+        setDate.put(10, new Integer[]{2, 2, 2, 5});
+        setDate.put(11, new Integer[]{2, 2, 2, 3, 2, 5});
+        setDate.put(12, new Integer[]{2, 2, 2, 4, 2, 5});
+
+        for (int i = 0; i < temp1.length; i = i + 2) {
+            hangeul[temp1[i]][temp1[i + 1]] =
+                    PURPLE_BOLD_BRIGHT + hangeul[temp1[i]][temp1[i + 1]] + RESET;
+        }
+    }
+
+
 
     public void ClockView() {
         for(String[] i : hangeul) {
             for (String j : i) {
-               System.out.print(j + " ");
+                System.out.print(j + " ");
             }
             System.out.println();
         }
